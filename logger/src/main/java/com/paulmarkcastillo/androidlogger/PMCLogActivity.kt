d810554178b81d7fdd06bb7953f3de7db6770435
Ricky Hadjirul <rhadjirul@isbx.com>
2019-05-20 12:34:37 +0800

    Fixed typos and fix issue in onResume

--- logger/src/main/java/com/paulmarkcastillo/androidlogger/PMCLogActivity.kt
+++ logger/src/main/java/com/paulmarkcastillo/androidlogger/PMCLogActivity.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
-import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -18,17 +17,18 @@ import dmax.dialog.SpotsDialog
 
 class PMCLogActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityPmclogBinding
     private lateinit var adapter: PMCLogAdapter
     private lateinit var progressDialog: AlertDialog
     private lateinit var spinnerAdapter: ArrayAdapter<String>
-    private lateinit var tag: String
+    private val tags = ArrayList<String>()
+    private var selectedTag: String = ""
     private  var showWithTag = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding: ActivityPmclogBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_pmclog)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_pmclog)
 
         progressDialog = SpotsDialog.Builder().setContext(this).build()
         progressDialog.setCancelable(false)
@@ -49,78 +49,94 @@ class PMCLogActivity : AppCompatActivity() {
         binding.recyclerviewLogs.adapter = adapter
 
         binding.buttonRefresh.setOnClickListener {
-            if (showWithTag) {
-                displayLogsWithSelectedTag(tag)
-            } else {
-                displayLogs()
-            }
+            refresh()
         }
 
         binding.buttonClear.setOnClickListener {
             deleteLogs()
         }
 
-        setupSpinnerAdapter(tagsList(), binding.tagsSpnr)
+        binding.spinnerTags.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
 
-        binding.tagsSpnr.onItemSelectedListener = object : OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
                 showWithTag = position > 0
-                tag = binding.tagsSpnr.selectedItem.toString()
+                selectedTag = binding.spinnerTags.selectedItem.toString()
+                if (showWithTag) {
+                    displayLogsWithSelectedTag(selectedTag)
+                } else {
+                    displayLogs()
+                }
             }
         }
+
+        setupSpinnerAdapter()
     }
 
     override fun onResume() {
         super.onResume()
-        displayLogs()
+        refresh()
     }
 
-    fun displayLogs() {
-        progressDialog.show()
-        val logs = PMCLogger.getLogsObservable()
-        logs.observe(this, Observer<List<PMCLog>> {
-            adapter.submitList(it)
-            progressDialog.dismiss()
-        })
+    private fun refresh() {
+        if (showWithTag) {
+            displayLogsWithSelectedTag(selectedTag)
+        } else {
+            displayLogs()
+        }
     }
 
-    fun deleteLogs() {
-        progressDialog.show()
-        val logs = PMCLogger.deleteLogs()
-        logs.observe(this, Observer<List<PMCLog>> {
-            adapter.submitList(it)
-            progressDialog.dismiss()
-        })
-    }
-
-    fun setupSpinnerAdapter(list: ArrayList<String>, spinner: Spinner) {
+    private fun setupSpinnerAdapter() {
         spinnerAdapter = ArrayAdapter(
             this,
             R.layout.spinner_item_layout,
-            list
+            tags
         )
-        spinner.adapter = spinnerAdapter
+        binding.spinnerTags.adapter = spinnerAdapter
     }
 
-    fun tagsList(): ArrayList<String> {
-        val tags = ArrayList<String>()
+    private fun displayLogs() {
+        progressDialog.show()
+        val logs = PMCLogger.getLogsObservable()
+        logs.observe(this, Observer<List<PMCLog>> {
+            if (it.isNotEmpty()) {
+                displayTags()
+            }
+            adapter.submitList(it)
+            progressDialog.dismiss()
+        })
+    }
+
+    private fun displayTags() {
         val result = PMCLogger.getAllTagsObservable()
         result.observe(this, Observer<List<String>> {
+            tags.clear()
             tags.addAll(it)
             spinnerAdapter.notifyDataSetChanged()
         })
-        return tags
     }
 
-    fun displayLogsWithSelectedTag(tag: String) {
+    private fun displayLogsWithSelectedTag(tag: String) {
         progressDialog.show()
         val logs = PMCLogger.getLogsWithTagObservable(tag)
         logs.observe(this, Observer<List<PMCLog>> {
             adapter.submitList(it)
+            progressDialog.dismiss()
+        })
+    }
+
+    private fun deleteLogs() {
+        progressDialog.show()
+        val logs = PMCLogger.deleteLogs()
+        logs.observe(this, Observer<List<PMCLog>> {
+            adapter.submitList(it)
+            showWithTag = false
+            spinnerAdapter.clear()
             progressDialog.dismiss()
         })
     }
