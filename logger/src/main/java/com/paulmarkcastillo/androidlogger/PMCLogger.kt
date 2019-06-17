87057e508db1736066d66e4732788813e5f417bd
John Lloyd Divino <divinojohnlloyd@gmail.com>
2019-06-17 08:16:53 +0800

    fixed csv format

--- logger/src/main/java/com/paulmarkcastillo/androidlogger/PMCLogger.kt
+++ logger/src/main/java/com/paulmarkcastillo/androidlogger/PMCLogger.kt
@@ -70,11 +70,13 @@ class PMCLogger {
         }
 
         private fun addLog(priority: Int, tag: String, msg: String) {
+            val formattedMsg = msg.replace(",", " ")
+
             if (!debugMode) {
                 val log = PMCLog(
                     priority = priority,
                     tag = tag,
-                    msg = msg
+                    msg = formattedMsg
                 )
                 CoroutineScope(Dispatchers.IO).launch {
                     val dao = PMCLogDatabase.getDatabase(applicationContext).logDao()
